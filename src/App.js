3d4520b5dcf0ab3f4f1b68611b4568e91c668aff
koen-benne <koen.benne@gmail.com>
2023-12-11 11:30:27 +0100

    automatically go to next page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { MemoryRouter, Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router';
 import Startup from './screens/Startup/Component';
 
 function App() {
   return (
     <div className="App">
-      <MemoryRouter>
         <Routes>
           <Route path="/" element={<Startup/>} />
           <Route path="about" element={<h1>About</h1>} />
         </Routes>
-      </MemoryRouter>
     </div>
   );
 }
--- src/screens/Startup/Component.js
+++ src/screens/Startup/Component.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './Style.module.css';
+import { useNavigate } from 'react-router';
 
-const Startup = () => {
+const Startup = (props) => {
+  const navigate = useNavigate();
+
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(prev => prev + 1);
+      if (timer > 1) {
+        navigate('/about', {replace: true});
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timer]);
+
   return (
     <div className={style.screen}>
-      <img src={process.env.PUBLIC_URL + '/logo.svg'} alt="Remobility" />
+      <img className={timer > 0 ? style.disappear : null} src={process.env.PUBLIC_URL + '/logo.svg'} alt="Remobility" />
     </div>
   );
 }
