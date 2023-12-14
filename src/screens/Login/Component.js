92cd9d033106b7f08cd095dc66305aa5aa7f953b
koen-benne <koen.benne@gmail.com>
2023-12-14 14:36:44 +0100

    update

--- src/screens/Login/Component.js
+++ src/screens/Login/Component.js
@@ -5,7 +5,7 @@ const Login = (props) => {
   return (
     <div className={style.screen}>
       <div className={style.left}>
-        <img className={style.logo} src={process.env.PUBLIC_URL + '/logo.svg'} alt="Remobility" />
+        <img className={style.logo} src={process.env.PUBLIC_URL + '/logo-colored.svg'} alt="Remobility" />
       </div>
       <div className={style.right} style={{backgroundImage: "url('" + process.env.PUBLIC_URL + "/login-image.png')"}}>
       </div>
