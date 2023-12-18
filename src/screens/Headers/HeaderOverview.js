f1b2ee7274aab926f4b8da42d803dd1207c1265f
Dannyhu2002 <55080994+Dannyhu2002@users.noreply.github.com>
2023-12-18 12:32:05 +0100

    Design changes in Dashboard and Location page
    - Switched Logout and Settings places
    - Removed the auto scroll bar
    - Changed text color of location bar

--- src/screens/Headers/HeaderOverview.js
+++ src/screens/Headers/HeaderOverview.js
@@ -7,16 +7,16 @@ import logoutIcon from '../../assets/logout.png';
 const HeaderOverview = () => {
   return (
     <header className="overview-header">
-      <button className="side-button">
-      <img src={logoutIcon} alt="Log out" className="iconlogout" />
-        Log uit
+        <button className="side-button">
+        <img src={gearIcon} alt="Settings" className="icongear" />
       </button>
       <div className="overview-header-content">
         <h1>IP-Car_123456</h1>
         <div className="online-indicator"></div>
       </div>
       <button className="side-button">
-        <img src={gearIcon} alt="Settings" className="icongear" />
+      <img src={logoutIcon} alt="Log out" className="iconlogout" />
+        Log uit
       </button>
     </header>
   );
