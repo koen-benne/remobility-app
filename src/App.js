a509c5bfb5f6c52a91fedc05d6922b7a2cca42fd
koen-benne <koen.benne@gmail.com>
2023-12-18 10:46:03 +0100

    fix issues with merge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { Routes, Route, useNavigate } from 'react-router';
 import { useEffect } from 'react';
-import './App.css';
-import { Routes, Route } from 'react-router';
 import Startup from './screens/Startup/Component';
 import Login from './screens/Login/Component';
 import Steps from './screens/Steps/Component';
 import Dashboard from './screens/Dashboard/Dashboard';
-import Startup from './screens/Startup/Component'; // Your original import
 import LiveLocation from './screens/LiveLocation/LocationPage'; // Adjust the path if necessary
 import Drive from './screens/Drive/DrivePage';
 
@@ -14,12 +11,10 @@ function App() {
 
   return (
     <div className="App">
-        <Routes>
-          <Route path="/" element={<Startup/>} />
-          <Route path="login" element={<Login/>} />
-          <Route path="welcome" element={<Steps/>} />
-        </Routes>
       <Routes>
+        <Route path="/" element={<Startup/>} />
+        <Route path="login" element={<Login/>} />
+        <Route path="welcome" element={<Steps/>} />
         <Route path="/" element={<Startup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/drive" element={<Drive />} /> {/* Route for the drive page */}
@@ -29,3 +24,4 @@ function App() {
   );
 }
 
+export default App;
