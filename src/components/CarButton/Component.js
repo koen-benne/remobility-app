5894937b94bea2ad25471101fa5f850f279bae4c
koen-benne <koen.benne@gmail.com>
2024-01-17 20:30:11 +0100

    add icons

--- src/components/CarButton/Component.js
+++ src/components/CarButton/Component.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import style from './Style.module.css';
 import { useRecoilState, atom } from 'recoil';
+import deleteIcon from '../../assets/deletecoloricon.png';
+import editIcon from '../../assets/editcoloricon.png';
 
 const currentCar = atom({
   key: 'currentCar',
@@ -11,8 +13,12 @@ const CarButton = (props) => {
   const [car, setCar] = useRecoilState(currentCar);
 
   return (
-    <div className={`${style.button} ${props.tag == car && style.active}`}>
+    <div className={`${style.button} ${props.tag == car && style.active}`} onClick={() => setCar(props.tag)}>
       <h2>IP-Car_{props.tag}</h2>
+      <div className={style.editButtons}>
+        <img src={deleteIcon} className={style.editButton}></img>
+        <img src={editIcon} className={style.editButton}></img>
+      </div>
     </div>
   );
 };
